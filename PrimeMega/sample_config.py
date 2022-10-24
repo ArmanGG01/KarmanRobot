0e5e724a1477f4e75aa5aacf52c8458197e9b3a6
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-10-24 14:13:53 +0700

    Update sample_config.py

--- PrimeMega/sample_config.py
+++ PrimeMega/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "excrybaby"
-    SUPPORT_CHAT = "PrimeSupportGroup"  # Your own group for support, do not add the @
+    OWNER_ID = 1694909518  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "PakkPoll"
+    SUPPORT_CHAT = "obrolansuar"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001302879778
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001528817258
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
