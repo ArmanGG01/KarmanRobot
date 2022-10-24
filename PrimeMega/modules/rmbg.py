1b23a78fdfbd459574a192670a13beb3a2c62c4f
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-10-24 14:09:12 +0700

    Update rmbg.py

--- PrimeMega/modules/rmbg.py
+++ PrimeMega/modules/rmbg.py
@@ -75,7 +75,7 @@ async def _(event):
         await event.reply("Background Removed in {} seconds".format(ms))
     else:
         await event.reply(
-            "remove.bg API returned Errors. Please report to @PrimeSupportGroup\n`{}".format(
+            "remove.bg API returned Errors. Please report to @obrolansuar\n`{}".format(
                 output_file_name.content.decode("UTF-8")
             )
         )
